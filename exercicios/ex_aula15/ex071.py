10e23543c423604fc462b4f9da02806322ca924f
PedroHenriqueGG <GITPedroHH@gmail.com>
2024-04-09 23:12:43 -0300

    exercicios da aula 15 terminados. inicio a ex da aula 16

--- exercicios/ex_aula15/ex071.py
+++ exercicios/ex_aula15/ex071.py
@@ -1,13 +1,41 @@
 print('/--ex71--/')
 
-print('=' * 30)
+print('=' * 35)
 print(' ' * 9, 'BANCO PHH')
-print('=' * 30)
+print('=' * 35)
 
-#50, 20, 10, 1
+valor = int(input('qual valor quer sacar?: \033[32mR$\033[m'))
+sed_100 = sed_50 = sed_10 = sed_5 = sed_1 = 0
 
-valor = int(input('que valor você quer sacar?: '))
-cinquenta = valor / 50
-vinte = cinquenta / 20
-dez = vinte / 10
-um = dez / 1
+# cédulas de 100.00R$
+while valor >= 100:
+    valor -= 100
+    sed_100 += 1
+
+# cédulas de 50.00R$
+while valor >= 50:
+    valor -= 50
+    sed_50 += 1
+
+# cédulas de 10.00R$
+while valor >= 10:
+    valor -= 10
+    sed_10 += 1
+
+# cédulas de 5.00R$
+while valor >= 5:
+    valor -= 5
+    sed_5 += 1
+
+# cédulas de 1.00R$
+while valor >= 1:
+    valor -= 1
+    sed_1 += 1
+
+print(f'Total de \033[33m{sed_100}\033[m céduas de R$100')
+print(f'Total de \033[33m{sed_50}\033[m cédulas de R$50')
+print(f'Total de \033[33m{sed_10}\033[m cédulas de R$10')
+print(f'Total de \033[33m{sed_5}\033[m cédulas de R$5')
+print(f'Total de \033[33m{sed_1}\033[m cédulas de R$1')
+print('=' * 35)
+print('Obrigado por usar o nosso banco. tenha um bom dia!')
---
+++ exercicios/ex_aula16/ex072.py
@@ -0,0 +1,9 @@
+print('/--ex72--/')
+
+frutas = ('laranja', 2, 'limoes', 100, 'goiaba')
+
+for c in frutas:
+    if c == frutas[2]:
+        print(c)
+    else:
+        print(frutas[0])
